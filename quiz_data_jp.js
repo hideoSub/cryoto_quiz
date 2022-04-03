56bff048bdf641946906de6001b8bdd064abbf23
hideoSub <101997887+hideoSub@users.noreply.github.com>
2022-04-03 11:04:52 +0900

    Update quiz_data_jp.js

--- quiz_data_jp.js
+++ quiz_data_jp.js
@@ -1,36 +1,44 @@
 const quizDataJp = [
   {
-    question: 'イギリスのバンド Blur の曲はどれ？',
-    a: 'Take My Hand',
-    b: 'When The Sun Goes Down',
-    c: 'Song 2',
-    d: 'Rewind',
+    question: '第１問　Solanaのローンチはいつですか',
+    a: '2009年1月',
+    b: '2015年7月',
+    c: '2020年3月 ',
+    // d: 'Rewind',
     correct: 'c'
   },
   {
-    question: 'オーストラリアのバンド The Vines の曲 \nGet Free に出てくる地名は？',
-    a: 'テキサス',
-    b: 'ニューヨーク',
-    c: 'オレゴン',
-    d: 'カリフォルニア',
-    correct: 'd'
+    question: '第２問　networkとnative token(network token)の正しい組合わせはどれですか',
+    a: 'Solana / SOL',
+    b: 'Ethereum / SOL',
+    c: 'bitcoin / bitcoin',
+    // d: 'カリフォルニア',
+    correct: 'a'
   },
   {
-    question: '■に入る文字はどれ？\n Smells ■■■■ Teen Spirit',
-    a: 'Nice',
-    b: 'Like',
-    c: 'Good',
-    d: 'Rock',
+    question: '第３問　「Transactions per second」は5万程度と言われていますが、実測値で1番近いのはどれですか',
+    a: '数百 TPS',
+    b: '数千 TPS',
+    c: '数十万 TPS',
+    // d: 'Rock',
     correct: 'b'
   },
   {
-    question: 'Saven Nation Army と言えば？',
-    a: 'White Stripe',
-    b: 'Blue Stripe',
-    c: 'Red Stripe',
-    d: 'Green Stripe',
+    question: '第４問　時価総額ランキング1番近いのはどれですか',
+    a: '10番目',
+    b: '50番目',
+    c: '80番目',
+    // d: 'Green Stripe',
     correct: 'a'
-  }
+  },
+  {
+    question: '第５問　コンセンサスアルゴリズムはどれですか',
+    a: 'PoW',
+    b: 'PoS',
+    c: 'PoH',
+    // d: 'Green Stripe',
+    correct: 'b'
+  },
 ];
 
 export { quizDataJp }
