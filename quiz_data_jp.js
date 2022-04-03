04fc6071c71f4122d4ea259d97cb622ff9f40edb
hideoSub <101997887+hideoSub@users.noreply.github.com>
2022-04-03 11:44:37 +0900

    Update quiz_data_jp.js

--- quiz_data_jp.js
+++ quiz_data_jp.js
@@ -4,7 +4,6 @@ const quizDataJp = [
     a: '2009年1月',
     b: '2015年7月',
     c: '2020年3月 ',
-    // d: 'Rewind',
     correct: 'c'
   },
   {
@@ -12,7 +11,6 @@ const quizDataJp = [
     a: 'Solana / SOL',
     b: 'Ethereum / SOL',
     c: 'bitcoin / bitcoin',
-    // d: 'カリフォルニア',
     correct: 'a'
   },
   {
@@ -20,7 +18,6 @@ const quizDataJp = [
     a: '数百 TPS',
     b: '数千 TPS',
     c: '数十万 TPS',
-    // d: 'Rock',
     correct: 'b'
   },
   {
@@ -28,7 +25,6 @@ const quizDataJp = [
     a: '10番目',
     b: '50番目',
     c: '80番目',
-    // d: 'Green Stripe',
     correct: 'a'
   },
   {
@@ -36,8 +32,38 @@ const quizDataJp = [
     a: 'PoW',
     b: 'PoS',
     c: 'PoH',
-    // d: 'Green Stripe',
     correct: 'b'
+  },
+  {
+    question: '第６問　token standardsはどれですか',
+    a: 'ERC20',
+    b: 'SPL',
+    c: 'BEP-20',
+    correct: 'b'
+  },{
+    question: '第７問　開発時に一般的に使用されているプログラミング言語はどれですか',
+    a: 'solidity',
+    b: 'motoko',
+    c: 'Rust',
+    correct: 'c'
+  },{
+    question: '第８問　Solana上のメタバースはどれですか',
+    a: 'Portals',
+    b: 'The Sandbox',
+    c: 'Decentraland',
+    correct: 'a'
+  },{
+    question: '第９問　1transaction/jouleで最も近いのはどれですか',
+    a: '2kJ',
+    b: '777.6MJ',
+    c: '7.4GJ',
+    correct: 'a'
+  },{
+    question: '第１０問　正しいのはどれですか',
+    a: 'SolPunksとCryptoPunksは関係がある',
+    b: 'BASC(Bored Ape Solana Club)とBAYC(Bored Ape Yadht Club)は関係がある',
+    c: '両方とも関係ない',
+    correct: 'c'
   },
 ];
 
